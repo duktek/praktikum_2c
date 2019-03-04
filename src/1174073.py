86df2945a16424b4ca3b0454372cebab357e355a
DirgaBrajamusti <dirgaizanam@gmail.com>
2019-03-05 02:23:56 +0700

    Tambahan punya ainul

---
+++ src/1174073.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+#no1
+
+
+ 
+print(1174073%3)
+print ("+++ +++  +++++++++++ +++   +++  +++++++++  +++++++++++  +++++++++++")
+print ("+++ +++  +++++++++++ +++   +++  +++++++++  +++++++++++  +++++++++++")
+print ("+++ +++         +++  +++   +++  ++     ++         +++            ++")
+print ("+++ +++        +++   +++   +++  ++     ++        +++             ++")
+print ("+++ +++       +++    +++++++++  ++     ++       +++              ++")
+print ("+++ +++      +++           +++  ++     ++      +++        +++++++++")
+print ("+++ +++     +++            +++  ++     ++     +++                ++")
+print ("+++ +++    +++             +++  ++     ++    +++                 ++")
+print ("+++ +++   +++              +++  ++     ++   +++                  ++")
+print ("+++ +++  +++               +++  +++++++++  +++          +++++++++++")
+print ("+++ +++ +++                +++  +++++++++ +++           +++++++++++")
+
+#nomor 2
+npm = input("Masukan NPM :")
+nilai = int(npm[5:7])
+hitung = 0
+while(hitung < nilai):
+ print("Halo, " +  str(npm) + " Apa Kabar?")
+ hitung = hitung +1
+
+# nomor 3
+npm = input("Masukan NPM :")
+nilai = int(npm[4:5]) + int(npm[5:6]) + int(npm[6:7]) 
+hitung = 0
+while(hitung < nilai):
+ print("Halo, " + str(npm[4:7]) + " Apa Kabar?")
+hitung = hitung +1
+
+#nomor 4
+npm = input("Masukan NPM :")
+print("Halo, " + str(npm[-3]) + " Apa Kabar?")
+
+#nomor 5
+
+a = 1
+b = 1
+c = 7
+d = 4
+e = 0
+f = 7
+g = 3
+h = a+b+c+d+e+f+g
+i = a*b*c*d*e*f*g
+print(a,b,c,d,e,f,g)
+
+#nomor 6
+
+print(h)
+
+#nomor 7
+
+print(i)
+
+#nomor 8
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+#nomor 9
+
+a = 1
+b = 1
+c = 7
+d = 3
+e = 0
+f = 7
+g = 3
+for n in npm:
+    if(n % 2 == 0):
+        if(n != 0):
+        print(n, end ="")
+
+
+#nomor 10
+
+a = 1
+b = 1
+c = 7
+d = 3
+e = 0
+f = 7
+g = 3
+for n in npm:
+    if(n % 2 != 0):
+        print(n, end ="")
+#nomor 11
+
+a = 1
+b = 1
+c = 7
+d = 3
+e = 0
+f = 7
+g = 3
+print(c)
+
+
+
+
+
+ 
+
+
+
+ 
+
+
+ 
+
