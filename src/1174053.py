59ca9c28309cfc2344d05fd1fc288f1d1b7d38f6
DirgaBrajamusti <dirgaizanam@gmail.com>
2019-03-05 08:58:31 +0700

    File baru

---
+++ src/1174053.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 19:28:20 2019
+
+@author: User
+"""
+
+print(1174053%3)
+print("  ***   ***  *********      ***    *****     ********   ******")
+print("  ***   ***  *********    *****   ***  ***   ********        **")
+print("  ***   ***      ****  ********  **     **   ***             **")
+print("  ***   ***      ***  ***   ***  **     **   *******    *****")
+print("  ***   ***     ***   *********  **     **        ***        **")
+print("  ***   ***    ***         ****  ***   ***       ****        **")
+print("  ***   ***   ***          ****   ******     *******    ******")
+
+a = input ("input")
+b = 0
+while(b < 53):
+    print("Halo, " + str(a) + " Apa Kabar ?")
+    b = b+1
+
+a = input ("input")
+b = 0
+while(b < 5):
+    print("Halo, " + str(a[4:7]) + " Apa Kabar ?")
+    b = b+1
+    
+a = input ("input")
+b = 0
+while(b < 1):
+    print("Halo, " + str(a[-3]) + " Apa Kabar ?")
+    b = b+1
+    
+a=1
+b=1
+c=7
+d=4
+e=0
+f=7
+g=0
+h=a+b+c+d+e+f+g
+i=a*b*C*d*e*f*g
+
+print(a,b,c,d,e,f,g)
+
+print(h)
+
+print(i)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
+print(d)
+
+print(a,b,c,f,g)
+
+print(c,f)
+
+
