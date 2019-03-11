609e57e5e46d877b3f92d1970e3ff48c2b2c7528
Alfadian <alfafianowen.ao@gmail.com>
2019-03-12 02:36:05 +0700

    owen

---
+++ src/m_1174091.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 19:20:22 2019
+
+@author: User
+"""
+
+import p_1174091
+
+x=100
+y=1000
+
+perkalian=p_1174091.perkalian(x,y)
---
+++ src/t_1174091.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 20:08:36 2019
+
+@author: User
+"""
+
+def fungsi(a,b):
+    c=a+b
+    return c
+
+a="nama saya "
+b="owen"
+
+jadi=fungsi(a,b)
+    
+from kalkulator_1174091 import tambah
+
+hasil=tambah(a,b)
