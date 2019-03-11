5d47037a65209784ff12cd82c5b6582507d00820
alvan <alvanzah99@gmail.com>
2019-03-12 03:50:35 +0700

    Alvan

---
+++ src/1174077/main.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 22:19:24 2019
+
+@author: ASUS
+"""
+
+import kalkulator
+
+a=100
+b=50
+
+hasil1=kalkulator.Penambahan(a,b)
+hasil2=kalkulator.Pengurangan(a,b)
+hasil3=kalkulator.Perkalian(a,b)
+hasil4=kalkulator.Pembagian(a,b)
+
+from Ngitung import Ngitung
+
+a=100
+b=50
+
+hitung = Ngitung(a, b)
+
+hasil1 = hitung.Penambahan()
+hasil2 = hitung.Pengurangan()
+hasil3 = hitung.Perkalian()
+hasil4 = hitung.Pembagian()
+
+lib = __import__('3lib')
+
+npm = "1174006"
+
+lib.printNPM(npm)
+lib.printNPMDuaDijit(npm)
+lib.printNPMTigaDijit(npm)
+lib.printNPMDigitKetiga(npm)
+lib.printNPMSatuPersatu(npm)
+lib.printNPMPenjumlahan(npm)
+lib.printNPMPerkalian(npm)
+lib.printNPMDijitGenap(npm)
+lib.printNPMDijitGanjil(npm)
+lib.printNPMDijitPrima(npm)
+print()
+from kelas3lib import kelas3lib
+
+npm = "1174006"
+
+k3lib = kelas3lib(npm)
+
+k3lib.printNPM()
+k3lib.printNPMDuaDijit()
+k3lib.printNPMTigaDijit()
+k3lib.printNPMDigitKetiga()
+k3lib.printNPMSatuPersatu()
+k3lib.printNPMPenjumlahan()
+k3lib.printNPMPerkalian()
+k3lib.printNPMDijitGenap()
+k3lib.printNPMDijitGanjil()
+k3lib.printNPMDijitPrima()
