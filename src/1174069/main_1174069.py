8c1c4f17bdee3d0f1c30a8ef501b22b4378e70f0
fannysha <fannyshafira17@gmail.com>
2019-03-11 23:54:27 +0700

    Tugas Chapter 3

---
+++ src/1174069/main_1174069.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 20:18:22 2019
+
+@author: FannyCantik
+"""
+
+from fungsi_fanny import penulisan
+#from 3lib_1174069 import penulisan
+print(penulisan(int(input("Masukan NPM nyaa :"))))
