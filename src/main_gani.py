41a7b8fe127cdcf0eaab4eac8da8f2edf6712bf6
jayker1 <muhamad.gani33@gmail.com>
2019-03-12 02:45:44 +0700

    Gani

---
+++ src/main_gani.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 16:14:54 2019
+
+@author: Advent
+"""
+from fungsi_gani import learn
+from 3lib_gani import learn
+
+print(learn(int(input("Masukan NPM :"))))
