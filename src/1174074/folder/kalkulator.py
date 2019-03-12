5aa01e497dadaabcf8e25787e6512b9602f10051
jayker1 <muhamad.gani33@gmail.com>
2019-03-12 14:58:19 +0700

    Mochamad Arifqi R Chapter3

---
+++ src/1174074/folder/kalkulator.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 20:48:10 2019
+
+@author: Aspire E15
+"""
+
+def Penambahan(a, b):
+    r = a + b
+    return r
+
+def Pengurangan(a, b):
+    r = a - b
+    return r
+
+def Perkalian(a, b):
+    r = a * b
+    return r
+
+def Pembagian(a, b):
+    r = a / b
+    return r
---
+++ src/1174074/halo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 20:10:22 2019
+
+@author: Aspire E15
+"""
+
+def halo(a, b):
+    r = a + b
+    return r
